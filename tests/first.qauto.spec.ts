a15e29ee35d3b0b7a64bab79675268ddce0ef11c
JuliaKalaman <kalaman.julia@gmail.com>
2024-06-26 18:04:57 +0200

    hw15-locators

--- tests/first.qauto.spec.ts
+++ tests/first.qauto.spec.ts
@@ -3,5 +3,4 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByRole('button', { name: 'Guest log in' })).toBeVisible()
-  await page.pause()
 });
